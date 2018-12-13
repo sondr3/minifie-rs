a475628ca2b7f06d7a29f901b5d8bd0ad95ab027
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-12-14 00:08:54 +0100

    Add a massive file and a test for it
    Nothing works anymore, oh no

---
+++ json/tests/test.rs
@@ -0,0 +1,21 @@
+use std::fs::File;
+use std::io::BufReader;
+use json::minify::Minify;
+use std::io::Read;
+
+fn read_file(filename: &str) -> String {
+    let file = File::open(filename).unwrap();
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents).unwrap();
+    contents
+}
+
+#[test]
+fn large() {
+    let large = read_file("./tests/fixtures/large.json");
+    let large_correct = read_file("./tests/fixtures/large_correct.json");
+    let minified = Minify::new(large.as_str());
+
+    assert_eq!(large_correct.as_str(), format!("{}", minified));
+}
