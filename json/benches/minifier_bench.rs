24cffe1ff881f39eb21234cf73fadf8cc9a2ba98
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-12-15 00:39:37 +0100

    Add throughput to benchmarking
    But it doesn't work yet, big woop

--- json/benches/minifier_bench.rs
+++ json/benches/minifier_bench.rs
@@ -8,14 +8,19 @@ fn criterion_benchmark(c: &mut Criterion) {
     let file = File::open("./tests/fixtures/large.json").unwrap();
     let mut buf_reader = BufReader::new(file);
     let mut string = String::new();
+    let bytes: Vec<_> = string.bytes().collect();
     buf_reader.read_to_string(&mut string).unwrap();
 
-    c.bench_function("minify", move |b| {
-        b.iter_with_setup(
-            || string.as_str(),
-            |contents| format!("{}", Minify::new(contents)),
-        )
-    });
+    c.bench(
+        "minify",
+        Benchmark::new(
+            "wat",
+            move |b| b.iter_with_setup(
+                || string.as_str(),
+                |contents| format!("{}", Minify::new(contents)),
+            ),
+        ).throughput(Throughput::Bytes(bytes.len() as u32)),
+    );
 }
 
 criterion_group!(benches, criterion_benchmark);
